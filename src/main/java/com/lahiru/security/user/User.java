a3aadded04fa7312201c8f5b93fad0750f6963a2
LahiruMadushann <lahirumadushandl@gmail.com>
2025-04-10 20:57:04 +0530

    implement user entity

---
+++ src/main/java/com/lahiru/security/user/User.java
@@ -0,0 +1,23 @@
+package com.lahiru.security.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
